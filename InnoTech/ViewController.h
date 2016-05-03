697fa638dc4fb352ac23b99812f5fa8c6dd481fa
Vladyslav Gusakov <vladyslavgusakov@gmail.com>
2016-05-02 21:31:44 -0400

    Initial Commit

---
+++ InnoTech/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InnoTech
+//
+//  Created by Vladyslav Gusakov on 5/2/16.
+//  Copyright © 2016 Vladyslav Gusakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
